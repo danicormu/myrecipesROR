2f9e114077d9deaf932df1b00d70c811165cef42
danicormu <danicormu@gmail.com>
2016-01-20 23:21:17 +0000

    admin

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,8 +1,8 @@
 class RecipesController < ApplicationController
     before_action :set_recipe, only: [:edit, :update, :show, :like]
     before_action :require_user, except: [:show, :index, :like]
-    before_action :require_user_like, only [:like]
-    before_action :require_same_user, only: [:edit, :update]
+    before_action :require_user_like, only: [:like]
+    before_action :require_same_user, only: [:edit, :update]        
     
     def index
         @recipes = Recipe.paginate(page: params[:page], per_page: 4)
@@ -65,7 +65,7 @@ class RecipesController < ApplicationController
         end
         
         def require_same_user
-            if current_user != @recipe.chef
+            if current_user != @recipe.chef and !current_user.admin?
                 flash[:danger] = "You can only edit your own recipes"
                 redirect_to recipe_path
             end
